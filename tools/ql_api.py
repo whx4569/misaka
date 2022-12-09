931d76bd255718e82c7fd95fe680caf83ac36ca2
院长 <32212075+yuanter@users.noreply.github.com>
2022-12-10 02:34:43 +0800

    新增获取config.sh环境
    新增获取config.sh环境

--- tools/ql_api.py
+++ tools/ql_api.py
@@ -4,10 +4,14 @@ import time
 import requests
 
 ql_auth_path = '/ql/data/config/auth.json'
+ql_config_path = '/ql/data/config/config.sh'
 #判断环境变量
 flag = 'new'
 if not os.path.exists(ql_auth_path):
     ql_auth_path = '/ql/config/auth.json'
+    ql_config_path = '/ql/config/config.sh'
+    if not os.path.exists(ql_config_path):
+        ql_config_path = '/ql/config/config.js'
     flag = 'old'
 # ql_auth_path = r'D:\Docker\ql\config\auth.json'
 ql_url = 'http://localhost:5600'
@@ -40,6 +44,76 @@ def get_envs(name: str = None) -> list:
     if j_data['code'] == 200:
         return j_data['data']
     return []
+
+
+# 查询环境变量+config.sh变量
+def get_config_and_envs(name: str = None) -> list:
+    params = {
+        't': int(time.time() * 1000)
+    }
+    #返回的数据data
+    data = []
+    if name is not None:
+        params['searchValue'] = name
+    res = requests.get(ql_url + '/api/envs', headers=__get__headers(), params=params)
+    j_data = res.json()
+    if j_data['code'] == 200:
+        data = j_data['data']
+    with open(ql_config_path, 'r', encoding='utf-8') as f:
+        while  True:
+            # Get next line from file
+            line  =  f.readline()
+            # If line is empty then end of file reached
+            if  not  line  :
+                break;
+            #print(line.strip())
+            exportinfo = line.strip()
+            #去除注释#行
+            exportinfolist = exportinfo.split("#")
+            #print('exportinfolistd的长度：{}'.format(len(exportinfolist)))
+            if len(exportinfolist) < 2 :
+                #去除首尾空格、单引号和双引号
+                exportinfo = exportinfolist[0].strip().replace("\"","").replace("\'","")
+            else:
+                exportinfo = ""
+            #list_all = re.findall(r'export[ ](.+?)', exportinfo,re.DOTALL)
+            #print('exportinfo数据：{}'.format(exportinfo))
+            #以空格分隔，数组0为export，数组1为后面需要的数据
+            list_all = exportinfo.split(" ")
+            #print('list_all数据：{}'.format(list_all))
+            if len(list_all) > 1:
+                #以=分割，查找需要的环境名字
+                tmp = list_all[1].split("=")
+                if len(tmp) > 1:
+                    #print('tmp数据：{}'.format(tmp))
+                    info = tmp[0]
+                    if name in info:
+                        data_tmp = []
+                        data_json = {
+                            'id': None,
+                            'value': tmp[1],
+                            'status': 0,
+                            'name': name,
+                            'remarks': "",
+                            'position': None,
+                            'timestamp': int(time.time()*1000),
+                            'created': int(time.time()*1000)
+                        }
+                        if flag == 'old':
+                            data_json = {
+                            '_id': None,
+                            'value': tmp[1],
+                            'status': 0,
+                            'name': name,
+                            'remarks': "",
+                            'position': None,
+                            'timestamp': int(time.time()*1000),
+                            'created': int(time.time()*1000)
+                            }
+                        #print('需要的数据：{}'.format(data_json))
+                        data.append(data_json)
+        #print('第二次配置数据：{}'.format(data))
+    return data
 
 
 # 新增环境变量
